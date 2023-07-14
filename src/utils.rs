21357cc5926103e0b012c5c75e25a34e13d61ae2
Ryan Ardito <ryan.m.ardito@gmail.com>
2023-07-14 13:18:16 -0600

    reordered utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,20 @@
 use rayon::prelude::*;
 use image::{DynamicImage, ImageBuffer, Rgba};
 
-pub fn render(x: f64, y: f64, width: usize, height: usize, base_view_height: f64, zoom: f64, iterations: u32) -> Vec<u32> {
-    let x_min = x - (2.0 / zoom);
-    let x_max = x + (2.0 / zoom);
-    let y_min = y - (base_view_height / zoom);
-    let y_max = y + (base_view_height / zoom);
+fn is_in_cardioid(x0: f64, y0: f64) -> bool {
+    let q = (x0 - 0.25).powi(2) + y0.powi(2);
+    q * (q + (x0 - 0.25)) < 0.25 * y0.powi(2)
+}
+
+fn is_in_bulb(x0: f64, y0: f64) -> bool {
+    (x0 + 1.0).powi(2) + y0.powi(2) < 0.0625
+}
+
+pub fn render(x_pos: f64, y_pos: f64, width: usize, height: usize, base_view_height: f64, zoom: f64, iterations: u32) -> Vec<u32> {
+    let x_min = x_pos - (2.0 / zoom);
+    let x_max = x_pos + (2.0 / zoom);
+    let y_min = y_pos - (base_view_height / zoom);
+    let y_max = y_pos + (base_view_height / zoom);
     let x_exp = (x_max - x_min) / (width - 1) as f64;
     let y_exp = (y_max - y_min) / (height - 1) as f64;
 
@@ -24,6 +33,26 @@ pub fn render(x: f64, y: f64, width: usize, height: usize, base_view_height: f64
             }
         });
     buffer
+}
+
+pub fn calc_pixel(x1: f64, y1: f64, iterations: u32) -> u32 {
+    if is_in_cardioid(x1, y1) || is_in_bulb(x1, y1) { return 0; }
+
+    let mut x2 = 0.0;
+    let mut y2 = 0.0;
+    let mut w = 0.0;
+    let mut i = 0;
+
+    while x2 + y2 <= 4.0 {
+        let x = x2 - y2 + x1;
+        let y = w - x2 - y2 + y1;
+        x2 = x * x;
+        y2 = y * y;
+        w = (x + y) * (x + y);
+        i += 1;
+        if i > iterations { return 0; }
+    }
+    i
 }
 
 pub fn save_image(buffer: Vec<u32>, width: u32, height: u32, file_path: &str, oversample: u32) -> Result<(), image::ImageError> {
@@ -55,33 +84,4 @@ pub fn get_color(value: u32, scale: f64, shift: u32) -> u32 {
             (r << 16) | (g << 8) | b
         }
     }
-}
-
-pub fn calc_pixel(x1: f64, y1: f64, iterations: u32) -> u32 {
-    if is_in_cardioid(x1, y1) || is_in_bulb(x1, y1) { return 0; }
-
-    let mut x2 = 0.0;
-    let mut y2 = 0.0;
-    let mut w = 0.0;
-    let mut i = 0;
-
-    while x2 + y2 <= 4.0 {
-        let x = x2 - y2 + x1;
-        let y = w - x2 - y2 + y1;
-        x2 = x * x;
-        y2 = y * y;
-        w = (x + y) * (x + y);
-        i += 1;
-        if i > iterations { return 0; }
-    }
-    i
-}
-
-fn is_in_cardioid(x0: f64, y0: f64) -> bool {
-    let q = (x0 - 0.25).powi(2) + y0.powi(2);
-    q * (q + (x0 - 0.25)) < 0.25 * y0.powi(2)
-}
-
-fn is_in_bulb(x0: f64, y0: f64) -> bool {
-    (x0 + 1.0).powi(2) + y0.powi(2) < 0.0625
 }
